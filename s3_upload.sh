12be9b8b5b8b469f8f561f542292771657c28a8b
Jeff Leong <jleong@appneta.com>
2020-03-26 12:15:52 -0700

    APM-6342 fix s3 upload command

--- s3_upload.sh
+++ s3_upload.sh
@@ -13,4 +13,4 @@ temp_role=$(aws sts assume-role \
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
-aws s3 cp /data/buildAgent/temp/buildTmp/ aws s3 cp /data/buildAgent/temp/buildTmp/bin/bgpalerter-linux-x64 ${S3_PATH} --acl public-read-write
+aws s3 cp /data/buildAgent/temp/buildTmp/bin/bgpalerter-linux-x64 ${S3_PATH} --acl public-read-write
